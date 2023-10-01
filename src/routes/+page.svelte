62879a0ae3f14054f194e84bcf62a94e9389df75
Ashures <ashuresyt@gmail.com>
2023-10-02 12:03:00 +1300

    Added log-in form basics

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    let username = "";
+    let token = "";
+
+    const handleSubmit = () => {
+        console.log("form test");
+    };
+</script>
+
+<div class="main">
+    <form class="login-form" on:submit|preventDefault={handleSubmit}>
+        <div class="form-row">
+            <input bind:value={username} type="text" name="username" id="login-username" placeholder="Username">
+        </div>
+        <div class="form-row">
+            <input bind:value={token} type="text" name="token" id="login-token" placeholder="Token">
+        </div>
+        <div class="form-row">
+            <input type="submit" value="Log-in">
+        </div>
+    </form>
+</div>
